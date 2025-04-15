656a0be4981114659e380c2fa656e183f0c210ff
rhlie <runar.lie@gmail.com>
2025-04-15 15:47:57 +0200

    making it dynamic

--- sketch_stuen.js
+++ sketch_stuen.js
@@ -22,11 +22,17 @@ const DISPLAY = 1;
 const LEFT_PULSE = 2;
 const NUM_MODES = LEFT_PULSE + 1;
 
-const howManyLines=25;
-const howManyBeziers=5;
-const howManyPolys=5;
+let howManyLines=0;
+let howManyBeziers=0;
+let howManyPolys=0;
+let howManyCircles=0;
+let howManyDots=0;
+let howManyTriangles=0;
+let allObjects = {"dots":[], "circles":[], "lines":[], "beziers":[], "polys":[], "triangles":[], "quads":[]};
+let objectCount= {"dots":0, "circles":0, "lines":0, "beziers":0, "polys":0, "triangles":0, "quads":0};
 let bezmaps=[]
 let polyMaps=[]
+let dots
 let startC, endC;
 
 let myFont;
@@ -41,43 +47,9 @@ function setup() {
   mic.start(); // Load the library 
   pMapper = createProjectionMapper(this);
   k=0;
+  textFont(myFont);
   // initialize empty lines
-  for (let i = 0; i < howManyLines; i++) {
-    let lineMap = pMapper.createLineMap();
-    lineMaps.push(lineMap);
-
-    // decrease line width for higher stairs (farther away)
-    linewidth=4
-    k=k+1
-    if (k==1) {
-        lineMap.lineW = linewidth;
-    }
-    if (k==2) {
-        lineMap.lineW = linewidth*2;
-    }
-    if (k==3) {
-        lineMap.lineW = linewidth*3;
-    }
-    if (k==4) {
-        lineMap.lineW = linewidth*4;
-        k=0;
-    }
-    
-
-    textFont(myFont);
-
-    // end cap display
-    // lineMap.setEndCapsOff();
-    // lineMap.setEndCapsOn();
-  }
-  for (let i=0; i < howManyBeziers; i++) {
-    let bezMap = pMapper.createBezierMap();
-    bezmaps.push(bezMap);
-  }
-  for (let i=0; i < howManyPolys; i++) {
-    let polyMap = pMapper.createPolyMap(5);
-    polyMaps.push(polyMap);
-  }
+  
   pMapper.load("maps/map.json");
 
   // initialize gradient colors
@@ -93,25 +65,42 @@ function draw() {
 
   // display gradient lines
   let index = 0;
-  for (const lineMap of lineMaps) {
-    let c = lerpColor(startC, endC, index / howManyLines);
+  for (const lineMap of allObjects.lines) {
+    let c = lerpColor(startC, endC, index/allObjects.lines.length );
     lineMap.displayNumber();
     getLineMode(lineMap, index++, c);
   }
-  for (const bezMap of bezmaps) {
-    let c = lerpColor(startC, endC, index / howManyLines);
+  for (const bezMap of allObjects.beziers) {
+    let c = lerpColor(startC, endC, index/allObjects.beziers.length );
     bezMap.display(c);
   }
-  for (const polyMap of polyMaps) {
-    let c = lerpColor(startC, endC, index / howManyLines);
+  for (const polyMap of allObjects.dots) {
+    let c = lerpColor(startC, endC, index/allObjects.dots.length  );
     polyMap.display(c);
   }
+  for (const polyMap of allObjects.circles) {
+    let c = lerpColor(startC, endC, index/allObjects.circles.length );
+    polyMap.display(c);
+  } 0
+  for (const polyMap of allObjects.triangles) {
+    let c = lerpColor(startC, endC, index/allObjects.triangles.length );
+    polyMap.display(c);
+  }
+  for (const polyMap of allObjects.quads) {
+    let c = lerpColor(startC, endC, index/allObjects.quads.length );
+    polyMap.display(c);
+  }
+  for (const polyMap of allObjects.polys) {
+    let c = lerpColor(startC, endC, index/allObjects.polys.length );
+    polyMap.display(c);
+  }
+
 
   displayFrameRate();
 }
 
 function getLineMode(l, index, c) {
-  let offset = (index / howManyLines) * 2 * PI;
+  let offset = (index / allObjects.lines.length) * 2 * PI;
   let percent = pMapper.getOscillator(3, offset);
   switch (lineMode) {
     case LEFT_PULSE:
@@ -151,6 +140,18 @@ function cycleLineMode(framesPerCycle) {
     lineMode %= NUM_MODES;
   }
 }
+function rainbow(pg) {
+    pg.clear();
+    pg.push();
+    pg.background("pink");
+    pg.colorMode(HSB, 255);
+  
+    for (let i = 0; i < 1000; i++) {
+      pg.stroke(i % 255, 255, 255);
+      pg.line(i, 0, i, 300);
+    }
+    pg.pop();
+  }
 
 function keyPressed() {
   switch (key) {
@@ -168,6 +169,100 @@ function keyPressed() {
     case "s":
       pMapper.save("map.json");
       break;
+
+    case "0":
+        //Create circle
+        console.log("creating dot");
+        thisMap=pMapper.createBezierMap(4)
+        size=prompt("Enter size", "100");
+        thisMap.width=size;
+        thisMap.height=size;
+        console.log(thisMap);
+        allObjects.circles.push(thisMap);
+        objectCount.circles++;
+        break;
+    case "1":
+        console.log("creating dot");
+        thisMap=pMapper.createBezierMap(2)
+        size=prompt("Enter size", "100");
+        thisMap.width=size;
+        thisMap.height=size;
+        allObjects.dots.push(thisMap);
+        objectCount.dots++;
+        break;
+        //create dot
+    case "2":
+       
+            let lineMap = pMapper.createLineMap();
+            
+
+            lineMap.lineW =  prompt("Enter line width", "10");;
+            allObjects.lines.push(lineMap);
+            objectCount
+            
+            break;
+            
+        
+            // end cap display
+            // lineMap.setEndCapsOff();
+            // lineMap.setEndCapsOn();
+          
+        //create line
+    case "3":
+        allObjects.triangles.push(pMapper.createTriMap(100, 100));
+        objectCount.triangles++;
+        break;
+        //create triangle
+    case "4":
+        allObjects.quads.push(pMapper.createQuadMap(100, 100));
+        objectCount.quads++;
+        break;
+        //create quad
+    case "5":
+        thisPoly=pMapper.createPolyMap(5)
+        size=prompt("Enter size", "100");
+        thisPoly.width=size;
+        thisPoly.height=size;
+        allObjects.polys.push(thisPoly);
+        objectCount.polys++;
+        break;
+        //create ploymap
+    case "6":
+            allObjects.polys.push(pMapper.createPolyMap(6));
+            objectCount.polys++;
+            break;
+
+    case "7":
+        allObjects.polys.push(pMapper.createPolyMap(7));
+        objectCount.polys++;
+        break;
+        //create poly
+    case "8":
+        allObjects.polys.push(pMapper.createPolyMap(8));
+        objectCount.polys++;
+        break;
+        //create poly
+    case "9":
+        allObjects.beziers.push(pMapper.createBezierMap(6));
+        objectCount.beziers++;
+        break;
+        //create bezier
+
+    case "r":
+        localStorage.setItem("items", JSON.stringify(objectCount));
+        break;
+    case "p":
+        objectCount=JSON.parse(localStorage.getItem("items"));
+
+        
+        console.log(objectCount);
+        break;
+    case "d":
+        localStorage.clear();
+        break;
+
+
+        
   }
 }
 
